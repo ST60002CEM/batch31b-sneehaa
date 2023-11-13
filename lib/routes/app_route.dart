3fa61e09886af1ff7908df331a81ce3a7c63e8e6
sneehaa <adhikarisneha0001@gmail.com>
2023-11-13 13:44:47 +0545

    adding routes

---
+++ lib/routes/app_route.dart
@@ -0,0 +1,16 @@
+
+
+class AppRoute {
+  AppRoute._();
+
+  static const String dashboardRoute = '/';
+  static const String signupRoute = '/signup';
+  static const String verificationRoute = '/verification';
+
+  static getApplicationRoute() {
+    return {
+      signupRoute: (context) => const SignupScreen(),
+      verificationRoute: (context) => const VerificationScreen(),
+    };
+  }
+}
