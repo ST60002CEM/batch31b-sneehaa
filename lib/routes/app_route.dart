31ce8187457bf65ea277add6cbb75b9b1ce4d189
sneehaa <adhikarisneha0001@gmail.com>
2023-11-17 09:02:06 +0545

    Updated app_routes

--- lib/routes/app_route.dart
+++ lib/routes/app_route.dart
@@ -1,4 +1,6 @@
+import 'dart:js';
 
+import 'package:bookaway/screens/login_screen.dart';
 
 class AppRoute {
   AppRoute._();
@@ -6,11 +8,13 @@ class AppRoute {
   static const String dashboardRoute = '/';
   static const String signupRoute = '/signup';
   static const String verificationRoute = '/verification';
+  static const String loginRoute = '/login';
 
   static getApplicationRoute() {
     return {
       signupRoute: (context) => const SignupScreen(),
       verificationRoute: (context) => const VerificationScreen(),
+      loginRoute: (context) => const LoginScreen(),
     };
   }
 }
