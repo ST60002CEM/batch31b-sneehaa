296d03d3a4696694a2325e8157a66fc90d5dd09f
sneehaa <adhikarisneha0001@gmail.com>
2023-11-17 13:10:09 +0545

    splash screen editing

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,22 +6,60 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  bool showText = false;
+
   @override
   void initState() {
     super.initState();
-    Future.delayed(
-      const Duration(seconds: 3),
-      () {
-        Navigator.pushReplacementNamed(context, '/login');
-      },
-    );
+    Future.delayed(const Duration(seconds: 2), () {
+      if (mounted) {
+        setState(() {
+          showText = true;
+        });
+      }
+
+      Future.delayed(const Duration(seconds: 3), () {
+        if (mounted) {
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Image.asset('assets/images/splash.gif'),
+        child: ListView(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    'assets/images/splash.jpg',
+                    fit: BoxFit.cover,
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
+                  const Positioned(
+                    bottom: 10,
+                    left: 20,
+                    child: Text(
+                      'BookAway',
+                      style: TextStyle(
+                        fontFamily: 'Agbalumo',
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 18, 18, 18),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
