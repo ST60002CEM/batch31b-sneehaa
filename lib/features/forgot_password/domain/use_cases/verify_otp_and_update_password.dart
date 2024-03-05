eafc2da17cebe8cd9dd2abdd146af26162138696
sneehaa <94774019+sneehaa@users.noreply.github.com>
2024-03-05 11:39:37 +0545

    Create verify_otp_and_update_password.dart

---
+++ lib/features/forgot_password/domain/use_cases/verify_otp_and_update_password.dart
@@ -0,0 +1,19 @@
+import 'package:bookaway/core/failure/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../repository/otp_repository.dart';
+
+final verifyOTPandUpdateUseCaseProvider =
+    Provider.autoDispose<VerifyOTPandUpdatePasswordUseCase>((ref) =>
+        VerifyOTPandUpdatePasswordUseCase(ref.watch(otpRepositoryProvider)));
+
+class VerifyOTPandUpdatePasswordUseCase {
+  final IOTPRepository _otpRepository;
+
+  VerifyOTPandUpdatePasswordUseCase(this._otpRepository);
+
+  Future<Either<Failure, bool>> verifyandUpdateOTP(
+      String email, String otp, String newPassword) {
+    return _otpRepository.verifyandUpdateOTP(email, otp, newPassword);
+  }
+}
